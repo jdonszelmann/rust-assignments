7d6d3fbf3c69ccf016a5cf697d43b2d04e4255df
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2022-05-26 13:56:47 +0200

    Small fixes

--- src/week1/types/all_about_vecs.rs
+++ src/week1/types/all_about_vecs.rs
@@ -2,7 +2,7 @@ use weblab::weblab;
 
 #[weblab(programming_assignment)]
 /// In this assignment, you will learn to work with the Vec type.
-/// A Vec is a re-sizable array, which can store any type of value. For example, a `Vec<i64>` is a list of `i64` values.
+/// A Vec is a resizable array, which can store any type of value. For example, a `Vec<i64>` is a list of `i64` values.
 /// You should implement each function according to the description provided in the comment above it.
 ///
 /// You are not allowed to change any function signatures.
@@ -13,7 +13,7 @@ mod assignment {
         use weblab::{solution_only, template_only};
 
         /// Insert the value `3` at the end of the given Vec.
-        pub fn insert_three_v1(vec: Vec<i64>) -> Vec<i64> {
+        pub fn insert_three(vec: Vec<i64>) -> Vec<i64> {
             template_only! { todo!() }
             solution_only! {
                 let mut vec = vec;
@@ -23,7 +23,7 @@ mod assignment {
         }
 
         /// Insert the value `3` at the end of the given Vec.
-        pub fn insert_three_v2(vec: &mut Vec<i64>) {
+        pub fn insert_three_mut_ref(vec: &mut Vec<i64>) {
             template_only! { todo!() }
             solution_only! {
                 vec.push(3);
@@ -31,7 +31,7 @@ mod assignment {
         }
 
         /// Return the length of the Vec
-        pub fn length_v1(vec: &Vec<i64>) -> usize {
+        pub fn length(vec: &Vec<i64>) -> usize {
             template_only! { todo!() }
             solution_only! {
                 vec.len()
@@ -40,7 +40,7 @@ mod assignment {
 
         /// Return the length of the Vec
         /// Note that the return type is now `i64`, don't change it.
-        pub fn length_v2(vec: &Vec<i64>) -> i64 {
+        pub fn length_i64(vec: &Vec<i64>) -> i64 {
             template_only! { todo!() }
             solution_only! {
                 vec.len() as i64
@@ -76,7 +76,8 @@ mod assignment {
             }
         }
 
-        /// Return the sum of all values that are at an even INDEX in the vec.
+        /// Return the sum of all values that are at an even position in the vec.
+        /// This means the element at position 0 (the first element), position 2 (the third element), etc...
         /// For example, the sum of all the even elements in `[3,4,8,2,5,0]` = 3 + 8 + 5 = 16
         pub fn sum_even_indices(vec: &Vec<i64>) -> i64 {
             template_only! { todo!() }
@@ -95,7 +96,7 @@ mod assignment {
 
         /// Add 1 to each value in the vec. Note that the vec is passed in by mutable reference.
         /// For example, the vec `[3,4,8,2,5,0]` should be changed to `[4,5,9,3,6,1]`
-        pub fn add_1_to_all_v1(vec: &mut Vec<i64>) {
+        pub fn add_1_to_all_mut_ref(vec: &mut Vec<i64>) {
             template_only! { todo!() }
             solution_only! {
                 for elem in vec {
@@ -104,13 +105,13 @@ mod assignment {
             }
         }
 
-        /// Add 1 to each value in the vec. Note that the vec is moved in.
+        /// Add 1 to each value in the vec. Note that the vec is given as an argument by value, and needs to be returned.
         /// This function should be implemented by calling the `add_1_to_all_v1` function above.
-        pub fn add_1_to_all_v2(vec: Vec<i64>) -> Vec<i64> {
+        pub fn add_1_to_all(vec: Vec<i64>) -> Vec<i64> {
             template_only! { todo!() }
             solution_only! {
                 let mut vec = vec;
-                add_1_to_all_v1(&mut vec);
+                add_1_to_all_mut_ref(&mut vec);
                 vec
             }
         }
@@ -124,51 +125,51 @@ mod assignment {
         use super::solution::*;
 
         #[test]
-        pub fn test_insert_three_v1() {
-            assert_eq!(insert_three_v1(vec![]), vec![3]);
-            assert_eq!(insert_three_v1(vec![2]), vec![2, 3]);
-            assert_eq!(insert_three_v1(vec![3]), vec![3, 3]);
+        pub fn test_insert_three() {
+            assert_eq!(insert_three(vec![]), vec![3]);
+            assert_eq!(insert_three(vec![2]), vec![2, 3]);
+            assert_eq!(insert_three(vec![3]), vec![3, 3]);
             assert_eq!(
-                insert_three_v1(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0]),
+                insert_three(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0]),
                 vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 3]
             );
         }
 
         #[test]
-        pub fn test_insert_three_v2() {
+        pub fn test_insert_three_mut_ref() {
             let mut vec = vec![];
-            insert_three_v2(&mut vec);
+            insert_three_mut_ref(&mut vec);
             assert_eq!(vec, vec![3]);
 
             let mut vec = vec![2];
-            insert_three_v2(&mut vec);
+            insert_three_mut_ref(&mut vec);
             assert_eq!(vec, vec![2, 3]);
 
             let mut vec = vec![3];
-            insert_three_v2(&mut vec);
+            insert_three_mut_ref(&mut vec);
             assert_eq!(vec, vec![3, 3]);
 
             let mut vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-            insert_three_v2(&mut vec);
+            insert_three_mut_ref(&mut vec);
             assert_eq!(vec, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 3]);
         }
 
         #[test]
-        pub fn test_length_v1() {
-            assert_eq!(length_v1(&vec![]), 0);
-            assert_eq!(length_v1(&vec![1]), 1);
-            assert_eq!(length_v1(&vec![1, 2]), 2);
-            assert_eq!(length_v1(&vec![1, 2, 3]), 3);
-            assert_eq!(length_v1(&vec![1, 2, 3, 4, 5]), 5);
+        pub fn test_length() {
+            assert_eq!(length(&vec![]), 0);
+            assert_eq!(length(&vec![1]), 1);
+            assert_eq!(length(&vec![1, 2]), 2);
+            assert_eq!(length(&vec![1, 2, 3]), 3);
+            assert_eq!(length(&vec![1, 2, 3, 4, 5]), 5);
         }
 
         #[test]
-        pub fn test_length_v2() {
-            assert_eq!(length_v2(&vec![]), 0);
-            assert_eq!(length_v2(&vec![1]), 1);
-            assert_eq!(length_v2(&vec![1, 2]), 2);
-            assert_eq!(length_v2(&vec![1, 2, 3]), 3);
-            assert_eq!(length_v2(&vec![1, 2, 3, 4, 5]), 5);
+        pub fn test_length_i64() {
+            assert_eq!(length_i64(&vec![]), 0);
+            assert_eq!(length_i64(&vec![1]), 1);
+            assert_eq!(length_i64(&vec![1, 2]), 2);
+            assert_eq!(length_i64(&vec![1, 2, 3]), 3);
+            assert_eq!(length_i64(&vec![1, 2, 3, 4, 5]), 5);
         }
 
         #[test]
@@ -201,31 +202,31 @@ mod assignment {
         }
 
         #[test]
-        pub fn test_add_1_to_all_v1() {
+        pub fn test_add_1_to_all_mut_ref() {
             let mut vec = vec![1, 2, 3, 4, 5];
-            add_1_to_all_v1(&mut vec);
+            add_1_to_all_mut_ref(&mut vec);
             assert_eq!(vec, vec![2, 3, 4, 5, 6]);
 
             let mut vec: Vec<i64> = vec![];
-            add_1_to_all_v1(&mut vec);
+            add_1_to_all_mut_ref(&mut vec);
             assert_eq!(vec, Vec::<i64>::new());
 
             let mut vec = vec![-88, 77, 23];
-            add_1_to_all_v1(&mut vec);
+            add_1_to_all_mut_ref(&mut vec);
             assert_eq!(vec, vec![-87, 78, 24]);
 
             let mut vec = vec![1212190, 111110];
-            add_1_to_all_v1(&mut vec);
+            add_1_to_all_mut_ref(&mut vec);
             assert_eq!(vec, vec![1212191, 111111]);
         }
 
         #[test]
-        pub fn test_add_1_to_all_v2() {
-            assert_eq!(add_1_to_all_v2(vec![1, 2, 3, 4, 5]), vec![2, 3, 4, 5, 6]);
-            assert_eq!(add_1_to_all_v2(vec![]), Vec::<i64>::new());
-            assert_eq!(add_1_to_all_v2(vec![-88, 77, 23]), vec![-87, 78, 24]);
+        pub fn test_add_1_to_all() {
+            assert_eq!(add_1_to_all(vec![1, 2, 3, 4, 5]), vec![2, 3, 4, 5, 6]);
+            assert_eq!(add_1_to_all(vec![]), Vec::<i64>::new());
+            assert_eq!(add_1_to_all(vec![-88, 77, 23]), vec![-87, 78, 24]);
             assert_eq!(
-                add_1_to_all_v2(vec![1212190, 111110]),
+                add_1_to_all(vec![1212190, 111110]),
                 vec![1212191, 111111]
             );
         }
