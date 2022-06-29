a677bc8812af34d662ae723cb35314a37705f94d
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2022-06-29 13:52:02 +0200

    Move around week 4

--- src/week4/bits/mod.rs
+++
@@ -1,5 +0,0 @@
-use weblab::weblab_folder;
-
-mod bcd;
-
-weblab_folder!("Bit Fiddling", bcd);
--- src/week4/mod.rs
+++ src/week4/mod.rs
@@ -1,6 +1,6 @@
-mod bits;
 mod cpu;
+mod bcd;
 
 use weblab::weblab_folder;
 
-weblab_folder!("week 4", bits, cpu);
+weblab_folder!("week 4", bcd, cpu);
